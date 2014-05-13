3b92c2c564e19beacd5246d83e928edb35954209
Johnson Denen <jdenen@manta.com>
2014-05-13 00:56:30 -0400

    execute spec buffers in different browsers

--- custom/init-ruby.el
+++ custom/init-ruby.el
@@ -22,16 +22,38 @@
 (setq inf-ruby-prompt-pattern "^\\[[0-9]+\\] pry\\((.*)\\)[>*\"'] *")
 
 ;;---------------------------------------------------------------------------
+;; Set BROWSER_TYPE and execute current buffer
+;;---------------------------------------------------------------------------
+(defun rspec-with-browser-type (browser)
+  "Run current spec on BROWSER."
+  (interactive "sBROWSER_TYPE: ")
+  (let ((command (format "BROWSER_TYPE=%s bundle exec rspec %s" browser (ruby-test-find-file))))
+        (setq default-directory (rspec-project-root))
+        (compilation-start command)))
+
+;;---------------------------------------------------------------------------
+;; Execute current buffer with Chrome
+;;---------------------------------------------------------------------------
+(defun run-desktop-spec ()
+  "Use Chrome to test current spec."
+  (interactive)
+  (rspec-with-browser-type "chrome"))
+
+;;---------------------------------------------------------------------------
+;; Execute current buffer with iPhone
+;;---------------------------------------------------------------------------
+(defun run-mobile-spec ()
+  "Use iPhone to test current spec."
+  (interactive)
+  (rspec-with-browser-type "iphone"))
+
+;;---------------------------------------------------------------------------
 ;; Keybindings
 ;;---------------------------------------------------------------------------
 (global-set-key (kbd "C-c C-p") 'inf-ruby)
-
-(defun rspec-with-browser-type (b)
-  "Run current spec on browser B."
-  (interactive "sBROWSER_TYPE: ")
-  (let ((command (format "BROWSER_TYPE=%s bundle exec rspec %s" b (ruby-test-find-file))))
-        (setq default-directory (rspec-project-root))
-        (compilation-start command)))
+(global-set-key (kbd "C-c r d") 'run-desktop-spec)
+(global-set-key (kbd "C-c r m") 'run-mobile-spec)
+(global-set-key (kbd "C-c r i") 'rspec-with-browser-type)
 
 (provide 'init-ruby)
 ;;; init-ruby.el ends here
