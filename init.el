487f162fec86f42f06a3aeadfbfdc44fcf2011cb
Johnson Denen <jdenen@manta.com>
2014-10-26 22:00:02 -0400

    better organization

--- init.el
+++ init.el
@@ -10,42 +10,21 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
-;;; Install/require PACKAGE
-(defun inst-pkg (package)
-  "Conditionally install the given PACKAGE."
+;;; use-pkg
+(defun use-pkg  (package)
+  "Install and/or require PACKAGE."
   (if (package-installed-p package)
       (require package)
-      t
+    t
     (progn
       (package-refresh-contents)
       (package-install package)
       (require package))))
 
-;;; Packages
-(inst-pkg 'helm)
-(inst-pkg 'helm-ls-git)
-(inst-pkg 'magit)
-(inst-pkg 'company)
-(inst-pkg 'expand-region)
-(inst-pkg 'guide-key)
-(inst-pkg 'inf-ruby)
-(inst-pkg 'yari)
-(inst-pkg 'smartparens)
-(inst-pkg 'powerline)
-(inst-pkg 'ample-theme)
-(inst-pkg 'ace-jump-mode)
-
-;;; Modes
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
-(winner-mode 1)
-(guide-key-mode 1)
-(helm-mode 1)
-(global-company-mode 1)
-(smartparens-global-mode 1)
-
 ;;; `helm'
+(use-pkg 'helm)
+(use-pkg 'helm-ls-git)
+(helm-mode 1)
 (require 'helm-config)
 (global-unset-key (kbd "C-x c"))
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
@@ -60,41 +39,82 @@
       helm-ff-search-library-in-sexp        t
       helm-scroll-amount                    8
       helm-ff-file-name-history-use-recentf t)
+(global-set-key (kbd "C-x m") 'helm-M-x)
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-set-key (kbd "C-c p") 'helm-browse-project)
+(global-set-key (kbd "C-x f") 'helm-for-files)
+
+;;; `perspective'
+(use-pkg 'perspective)
+(persp-mode 1)
 
 ;;; `smartparens'
+(use-pkg 'smartparens)
+(smartparens-global-mode 1)
 (require 'smartparens-config)
 
 ;;; `guide-key-mode'
+(use-pkg 'guide-key)
+(guide-key-mode 1)
 (setq guide-key/guide-key-sequence '("C-x" "C-c"))
 (setq guide-key/idle-delay 2.0)
 (setq guide-key/recursive-key-sequence-flag t)
 
 ;;; `ruby-mode'
+(use-pkg 'inf-ruby)
+(use-pkg 'yari)
 (defun ri-bind()
   "Bind yari to 'ruby-mode'."
   (local-set-key (kbd "C-c y") 'yari-helm))
 (add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
 (add-hook 'ruby-mode-hook 'ri-bind)
 
-;;; Keybindings
-(global-set-key (kbd "C-=") 'er/expand-region)
-(global-set-key (kbd "C-x m") 'helm-M-x)
-(global-set-key (kbd "C-c h") 'helm-command-prefix)
-(global-set-key (kbd "C-c p") 'helm-browse-project)
-(global-set-key (kbd "C-x f") 'helm-for-files)
-(global-set-key (kbd "C-c C-c") 'company-complete)
-(global-set-key (kbd "C-x j") 'ace-jump-char-mode)
+;;; `magit'
+(use-pkg 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;;; Misc
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq backup-directory-alist '(("." . "~/tmp")))
+;;; `company'
+(use-pkg 'company)
+(global-company-mode 1)
+(global-set-key (kbd "C-c C-c") 'company-complete)
+
+;;; `expand-region'
+(use-pkg 'expand-region)
+(global-set-key (kbd "C-=") 'er/expand-region)
+
+;;; `powerline'
+(use-pkg 'powerline)
 (powerline-default-theme)
+
+;;; `ace-jump-mode'
+(use-pkg 'ace-jump-mode)
+(global-set-key (kbd "C-x j") 'ace-jump-char-mode)
+
+;;; `winner-mode'
+(winner-mode 1)
+
+;;; `ample-theme'
+(use-pkg 'ample-theme)
+(load-theme 'ample t)
+
+;;; visual
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
 (add-to-list 'default-frame-alist '(font .  "Droid Sans Mono-10" ))
 (set-face-attribute 'default t :font  "Droid Sans Mono-10" )
+
+;;; start up
 (setq inhibit-startup-screen t)
 (setq initial-major-mode 'ruby-mode)
-(load-theme 'ample t)
+(setq initial-scratch-message "\
+# This buffer is for notes you don't want to save
+# and Ruby code you'll throw away. Go Cavs.")
+(setq initial-buffer-choice "~/Code/notes.org")
+
+;;; misc
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq backup-directory-alist '(("." . "~/tmp")))
 
 (provide 'init)
 ;;; init.el ends here
