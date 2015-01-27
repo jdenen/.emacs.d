5e3144672d114101be7c965e0213db0eb4ab26f0
Johnson Denen <jdenen@manta.com>
2015-01-27 10:42:02 -0500

    use ace-isearch

--- init.el
+++ init.el
@@ -62,7 +62,7 @@
   :init (projectile-global-mode t))
 
 ;;;
-;;; helm search and navigation
+;;; helm
 ;;;
 (johnson/package-install 'helm)
 (use-package helm
@@ -88,9 +88,15 @@
   ("M-y"   . helm-show-kill-ring)
   ("C-x h" . helm-command-prefix))
 
+;;;
+;;; ace-isearch
+;;;
 (johnson/package-install 'helm-swoop)
-(use-package helm-swoop
-  :bind ("C-s" . helm-swoop))
+(johnson/package-install 'ace-isearch)
+(use-package ace-isearch
+  :init
+  (global-ace-isearch-mode 1)
+  (setq ace-isearch-input-length 1))
 
 (johnson/package-install 'helm-projectile)
 (use-package helm-projectile
@@ -164,7 +170,7 @@
   :init
   (progn
     (key-chord-mode 1)
-    (key-chord-define-global "jj" 'ace-jump-char-mode)  
+;;    (key-chord-define-global "jj" 'ace-jump-char-mode)  
     (key-chord-define-global "jr" 'jump-to-register)
     (key-chord-define-global "jg" 'magit-status)
     (key-chord-define-global "jp" 'helm-projectile)
