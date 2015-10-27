18c55cc69871790baf89f104154ac49fcaae894d
Johnson Denen <johnson.denen@gmail.com>
2015-10-27 08:58:18 -0400

    Add init.el custom changes

--- init.el
+++ init.el
@@ -8,3 +8,15 @@
 		   user-emacs-directory))
 (put 'narrow-to-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Documents/org-docs/johnson.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
