e88833738684c32ab139672a15d1b44cf15da911
Johnson Denen <johnson.denen@gmail.com>
2016-05-24 22:49:53 -0400

    Agenda reads from scratch.org

--- init.el
+++ init.el
@@ -13,7 +13,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/vc-docs/johnson.org") ("~/vc-docs/grad.org"))))
+ '(org-agenda-files (quote ("~/vc-docs/scratch.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
