5acffc2c3d75a0f525ccd391ae3f9c436b85a299
Johnson Denen <johnson.denen@gmail.com>
2016-09-21 09:34:36 -0400

    Fix groovy/gradle-* functions

--- layers/groovy/config.el
+++ layers/groovy/config.el
@@ -17,26 +17,30 @@
   "Execute gradle tasks with --daemon flag if non-nil.")
 
 (defun groovy//gradle (task)
-  "Execute gradle TASK.
+  "Execute TASK with gradle.
 
-Execute TASK with --daemon flag if `groovy-gradle-daemon' is non-nil."
+Execute with --daemon flag if `groovy-gradle-daemon' is non-nil."
   (let ((gradle-task (if groovy-gradle-daemon
                          (concat "gradle-" task "--daemon")
                        (concat "gradle-" task))))
-    (call-interactively '(intern gradle-task))))
+    (call-interactively (intern gradle-task))))
 
 (defun groovy/gradle-build ()
   "Execute gradle build task."
+  (interactive)
   (groovy//gradle "build"))
 
 (defun groovy/gradle-task ()
   "Execute a gradle task."
+  (interactive)
   (groovy//gradle "task"))
 
 (defun groovy/gradle-test ()
   "Execute gradle test task."
+  (interactive)
   (groovy//gradle "test"))
 
 (defun groovy/gradle-single-test ()
   "Execute gradle test task with a -Dtest.single= argument."
+  (interactive)
   (groovy//gradle "single-test"))
