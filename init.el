e307422c81e25e01e3822e18e26b485d775907c0
Johnson Denen <johnson.denen@gmail.com>
2016-09-21 09:14:38 -0400

    Set groovy-home variable

--- init.el
+++ init.el
@@ -248,6 +248,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (setq groovy-home "/home/jqd29/.sdkman/candidates/groovy/current")
   (global-linum-mode t)
   (setq gnus-use-full-window nil)
   (load "~/vc-docs/defs.el"))
