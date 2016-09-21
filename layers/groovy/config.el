1e278ed42f8f1d730618897f360b632ef9c42f8c
Johnson Denen <johnson.denen@gmail.com>
2016-09-21 08:40:56 -0400

    Convert to spacemacs

---
+++ layers/groovy/config.el
@@ -0,0 +1,50 @@
+;;; packages.el --- Groovy layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Johnson Denen <johnson.denen@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(defvar groovy-path-to-groovy-home nil
+  "Absolute path to a Groovy.")
+
+(defvar groovy-gradle-daemon nil
+  "Execute gradle tasks with --daemon flag if non-nil.")
+
+(defun groovy//inf-groovy-home-hook ()
+  "Set `groovy-home'."
+  (if groovy-path-to-groovy-home
+      (setq groovy-home groovy-path-to-groovy-home)))
+
+(defun groovy//gradle (task)
+  "Execute gradle TASK.
+
+Execute TASK with --daemon flag if `groovy-gradle-daemon' is non-nil."
+  (let ((gradle-task (if groovy-gradle-daemon
+                         (concat "gradle-" task "--daemon")
+                       (concat "gradle-" task))))
+    (call-interactively (intern gradle-task))))
+
+(defun groovy/gradle-build ()
+  "Execute gradle build task."
+  (groovy//gradle "build"))
+
+(defun groovy/gradle-task ()
+  "Execute a gradle task."
+  (groovy//gradle "task"))
+
+(defun groovy/gradle-test ()
+  "Execute gradle test task."
+  (groovy//gradle "test"))
+
+(defun groovy/gradle-single-test ()
+  "Execute gradle test task with a -Dtest.single= argument."
+  (groovy//gradle "single-test"))
---
+++ layers/groovy/packages.el
@@ -0,0 +1,49 @@
+;;; packages.el --- Groovy layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2016 Sylvain Benner & Contributors
+;;
+;; Author: Johnson Denen <johnson.denen@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;;; Code:
+
+(defconst groovy-packages
+  '(
+    gradle-mode
+    groovy-mode
+    ))
+
+(defun groovy/init-gradle-mode ()
+  (use-package gradle-mode
+    :defer t))
+
+(defun groovy/init-groovy-mode ()
+  (use-package groovy-mode
+    :defer t
+    :config
+    (progn
+      (add-hook 'groovy-mode-hook 'gradle-mode)
+      (add-hook 'groovy-mode-hook 'groovy//inf-groovy-home-hook)
+      (spacemacs/declare-prefix-for-mode 'groovy-mode "ms" "send")
+      (spacemacs/declare-prefix-for-mode 'groovy-mode "mc" "build")
+      (spacemacs/declare-prefix-for-mode 'groovy-mode "mt" "test")
+      (spacemacs/set-leader-keys-for-major-mode 'groovy-mode
+        "sb" 'groovy-load-file
+        "si" 'run-groovy
+        "sf" 'groovy-send-definition
+        "sF" 'groovy-send-definition-and-go
+        "sr" 'groovy-send-region
+        "sR" 'groovy-send-region-and-go
+        "cc" 'groovy/gradle-build
+        "ct" 'groovy/gradle-task
+        "ta" 'groovy/gradle-test
+        "ts" 'groovy/gradle-test
+        "tt" 'groovy/gradle-single-test))))
+
+;;; packages.el ends here
