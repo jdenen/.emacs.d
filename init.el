5b197f9036e5345b68f11ea15868c1de48e72bf9
Johnson P Denen <jdenen@cas.org>
2016-12-07 11:13:19 -0500

    Use Droid Sans Mono

--- init.el
+++ init.el
@@ -158,8 +158,8 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Source Code Pro"
-                               :size 13
+   dotspacemacs-default-font '("Droid Sans Mono"
+                               :size 11
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
